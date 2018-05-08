b33ef0f3bab92c9232db1a8dd112e43e470f094b
Brandon Green <brandon@brandonleeproductions.com>
2018-05-08 03:12:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
     if index.to_i.between?(1, 9)
-		position_taken?(board, index)
+		  position_taken?(board, index)
 		return true
 	else
 		return false
