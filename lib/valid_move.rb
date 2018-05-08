eb0dc4a133d1203f393d3c6384b2b602fc2d2949
Brandon Green <brandon@brandonleeproductions.com>
2018-05-08 03:46:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,3 @@
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false 
-  else
-    true
-  end
-end
-
 # code your #valid_move? method here
 def valid_move?(board, index)
    if index.to_i.between?(0, 8) # Need to know why if this is (1, 9) I get two errors vs. one error with (0, 8)?
