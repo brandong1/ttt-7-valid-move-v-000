9ea655602e8a3079ad104774270fc706cd01368b
Brandon Green <brandon@brandonleeproductions.com>
2018-05-08 03:30:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
     if index.to_i.between?(0, 8)
 		  position_taken?(board, index)
 		true
-  else if index.between(0, 8) && !position_taken?(board, index) == false
+  else if index.between(0, 8) && position_taken?(board, index) == false
 		false
 	end
  
