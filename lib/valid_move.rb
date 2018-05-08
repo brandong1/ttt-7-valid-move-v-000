02808d38f9d7a39e691da098981ec2d0b4b3c664
Brandon Green <brandon@brandonleeproductions.com>
2018-05-08 03:53:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
      # Need to know why if this is (1, 9) I get two errors vs. one error with (0, 8)?
 		  position_taken?(board, index)
 		true
-  else if board[index] !== " " || board[index] !== "" || board[index] !== nil
+  else if board[index] != " " || board[index] != "" || board[index] != nil
 		false
 	end
  
