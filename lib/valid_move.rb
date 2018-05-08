72e45188bf1c4a91036bb040867b5a6d1e500d95
Brandon Green <brandon@brandonleeproductions.com>
2018-05-08 03:38:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if index.to_i.between?(0, 8) # Need to know why if this is (1, 9) I get two errors vs. one error with (0, 8)?
-		  position_taken?(board, index)
+    index.to_i.between?(0, 8) # Need to know why if this is (1, 9) I get two errors vs. one error with (0, 8)?
+		if !position_taken?(board, index) && index.between(0, 8)
 		true
   else
 		false
